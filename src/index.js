ac97f1c8e2048fff831c73c163e0c6ad8ee44691
Megan Horsfield <meganthadev@gmail.com>
2020-08-30 04:32:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function createStore(candyReducer) {
   let state;
   
   function dispatch(action) {
-    state = reducer(state, action);
+    state = candyReducer(state, action);
     render();
   }
  
